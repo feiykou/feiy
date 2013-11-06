83ce396f911a7e670157dcb16dec86a3c95ac052
pyxyzn <pyxyzn@127.0.0.1>
2013-11-07 01:42:56 +0800

    修改名称

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Technicas OA',
+	'name'=>'technicas oa',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
