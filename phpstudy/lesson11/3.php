08dffb930553742c91b470bc6604024529b7bcc6
Administrator <Administrator@USER-20161011LV>
2017-07-24 17:23:40 +0800

    2017-7-24

--- phpstudy/lesson11/3.php
+++ phpstudy/lesson11/3.php
@@ -14,11 +14,18 @@ class person{
     }
     
     function __get($key){
-        echo "外部调用私有或者受保护的属性的时候自动触发<br>";
+        echo "外部调用私有或者受保护的属性的时候自动触发__get<br>";
         return $this->$key;
+    }
+    
+    function __set($key,$val){
+        echo "外部修改私有或者受保护的属性时候自动触发__set<br>";
+        return $this->$key = $val;
     }
 }
 
 $person = new person();
-// echo $person->name;
+echo $person->name;
+$person->sex = "看身份证";
 echo $person->sex;
+echo $person->name = "1";
