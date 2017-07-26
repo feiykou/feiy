a1e04dfb0454a0bd7c36029c806a512d07b23427
Administrator <Administrator@USER-20161011LV>
2017-07-26 17:30:10 +0800

    数据库操作

--- phpstudy/lesson14/1.php
+++ phpstudy/lesson14/1.php
@@ -1,1 +1,21 @@
 <?php
+/*
+ * mysqli 是mysql的增强版本
+ * 
+ */
+
+@$mysqli = new mysqli("127.0.0.1","root","","stu");
+if($mysqli->connect_errno){
+    die('服务器连接失败！');
+}
+$mysqli->set_charset("utf8");
+$sql = "select * from `stu`";
+$result = $mysqli->query($sql);
+$arr = array();
+while ($row = $result->fetch_assoc()){
+    $arr[] = $row;
+}
+
+echo "<pre>";
+var_dump($arr);
+
--- phpstudy/lesson14/2.php
+++ phpstudy/lesson14/2.php
@@ -1,1 +1,23 @@
 <?php
+
+$db = new PDO("mysql:host=localhost;dbname=stu","root","");
+$sql = "select * from `stu`";
+$result = $db->query($sql);
+$arr = array();
+while ($row = $result->fetch(PDO::FETCH_ASSOC)){
+    $arr[] = $row;
+}
+
+echo "<pre>";
+var_dump($arr);
+
+
+
+
+
+
+
+
+
+
+
